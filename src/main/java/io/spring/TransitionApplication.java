2778b2304976dbdcf8d5b5b351d0c57e7d730226
martinfou <martinfou@gmail.com>
2016-09-25 21:09:24 -0400

    initial import

---
+++ src/main/java/io/spring/TransitionApplication.java
@@ -0,0 +1,16 @@
+package io.spring;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class TransitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitionApplication.class,args);
+
+	}
+
+}
---
+++ src/main/java/io/spring/configuration/StepTransitionConfiguration.java
@@ -0,0 +1,8 @@
+package io.spring.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StepTransitionConfiguration {
+
+}
