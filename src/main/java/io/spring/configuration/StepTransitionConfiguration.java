51587c0372a5322c1ff89e4efd80ce7b193407b6
martinfou <martinfou@gmail.com>
2016-09-25 21:19:10 -0400

    adding transition application

--- src/main/java/io/spring/configuration/StepTransitionConfiguration.java
+++ src/main/java/io/spring/configuration/StepTransitionConfiguration.java
@@ -1,8 +1,69 @@
 package io.spring.configuration;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StepTransitionConfiguration {
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public Step step1(){
+		return stepBuilderFactory.get("step1")
+				.tasklet(new Tasklet() {
+					@Override
+					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContest)
+							throws Exception {
+						System.out.println("I am running");
+						return RepeatStatus.FINISHED;
+					}
+				}).build();
+	}
+	
+	@Bean
+	public Step step2(){
+		return stepBuilderFactory.get("step2")
+				.tasklet(new Tasklet() {
+					@Override
+					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContest)
+							throws Exception {
+						System.out.println("I am running");
+						return RepeatStatus.FINISHED;
+					}
+				}).build();
+	}
+	
+	@Bean
+	public Step step3(){
+		return stepBuilderFactory.get("step3")
+				.tasklet(new Tasklet() {
+					@Override
+					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContest)
+							throws Exception {
+						System.out.println("I am running");
+						return RepeatStatus.FINISHED;
+					}
+				}).build();
+	}
+	
+	public Job transiJobSimpleNext(){
+		return jobBuilderFactory.get("transitionJobNext")
+				.start(step1())
+				.next(step2())
+				.next(step3())
+				.build();
+	}
 
 }
